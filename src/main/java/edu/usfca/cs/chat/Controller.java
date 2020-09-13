53d3d66968c066a0e75569d0ee310441cfc08e71
Nishant Mehta <nishmeht7@gmail.com>
2020-09-13 12:56:26 -0700

    controller class added

---
+++ src/main/java/edu/usfca/cs/chat/Controller.java
@@ -0,0 +1,13 @@
+package edu.usfca.cs.chat;
+
+import java.util.Map;
+
+public class Controller {
+    // active nodes map with key being IP/hostname and value being directory path
+    private Map<String, String> activeNodes;
+    // add File system tree (binary tree?)
+    // add routing table
+
+
+
+}
