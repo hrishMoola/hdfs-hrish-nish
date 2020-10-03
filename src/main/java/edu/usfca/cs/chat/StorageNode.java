5ec4087e36ce64b1a7f503628af5d2e5a2362f13
Nishant Mehta <nishmeht7@gmail.com>
2020-10-02 23:26:35 -0700

    fixed retrieve and added separate flow

--- src/main/java/edu/usfca/cs/chat/StorageNode.java
+++ src/main/java/edu/usfca/cs/chat/StorageNode.java
@@ -147,15 +147,15 @@ public class StorageNode
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) {
-        /* A connection has been established */
-        InetSocketAddress addr
-            = (InetSocketAddress) ctx.channel().remoteAddress();
-        System.out.println("Connection established: " + addr);
-    }
+            /* A connection has been established */
+            InetSocketAddress addr
+                = (InetSocketAddress) ctx.channel().remoteAddress();
+            System.out.println("Connection established: " + addr);
+        }
 
-    @Override
-    public void channelInactive(ChannelHandlerContext ctx) {
-        /* A channel has been disconnected */
+        @Override
+        public void channelInactive(ChannelHandlerContext ctx) {
+            /* A channel has been disconnected */
         InetSocketAddress addr
             = (InetSocketAddress) ctx.channel().remoteAddress();
         System.out.println("Connection lost: " + addr);
@@ -190,7 +190,8 @@ public class StorageNode
                 break;
             case 2: // File Ack
                 try{
-                    System.out.println("File ACK received in storage node");
+                    String filePath = message.getFileAck().getFilepath();
+                    System.out.println("File ACK received in storage node for filepath: " + filePath);
                     //send metadata first if I have it?
                     //then send the chunks
                     DfsMessages.FileAck.Type ackMsgType = message.getFileAck().getType();
@@ -250,8 +251,10 @@ public class StorageNode
 
 
     private void getAndSendChunks(ChannelHandlerContext ctx, String filepath) throws IOException {
+        System.out.println("inside getAndSendchunks filepath: " + filepath);
         String storageDirectory = this.storagePath + "/original/";
 
+        System.out.println("dfs filepath: " + storageDirectory + filepath);
         File dir = new File(storageDirectory + filepath);
         List<DfsMessages.FileChunk> chunks = new ArrayList<>();
         for (File eachFile: dir.listFiles()) {
